8c4f7627803349401780c8f021bc6b9eff4ceff4
Francisco <sernaojuliofranciscojavier@gmail.com>
2025-09-09 10:48:18 -0500

    terminando configuraciones 4

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  base: process.env.VITE_BASE_PATH || "/popuesta_libro_frontend",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
